dcd4c766e583ef7a989259bdaea17222198ebd65
Abheek Dhawan <adawesomeguy222@gmail.com>
2020-10-07 06:05:05 +0000

    Remove excess files

--- website/home/images/heic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILES=*.JPG
-for f in $FILES
-do
-  f=${f%".JPG"}
-  echo "Processing $f file..."
-  convert "${f}.JPG" "${f}.jp2"
-done
--- website/home/images/jpg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-NUM=989;
-JPG=*.jpg;
-for j in $JPG; 
-do 
-    mv "$j" "JPG_$NUM.jpg"
-    NUM=$((NUM + 1 )); 
-done
